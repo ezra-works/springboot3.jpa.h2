f1c734682b6362ae2e7b4af7ec41ec8d8a88266e
Ezra Rajkumar <44539629+medavakkam@users.noreply.github.com>
2023-03-01 22:10:48 +0530

    First commit

---
+++ src/main/java/com/example/springboot3/jpa/h2/Application.java
@@ -0,0 +1,20 @@
+package com.example.springboot3.jpa.h2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @RequestMapping("/")
+    String landing() {
+        return "Landing page";
+    }
+}
