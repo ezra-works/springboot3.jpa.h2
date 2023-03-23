600ee7c5fe2a080cae6ee27c7459e6aea4efd2f7
Ezra Rajkumar <44539629+medavakkam@users.noreply.github.com>
2023-03-23 21:45:50 +0530

    [commit-5.2] adding WebMvcTest test for RestController

--- src/test/java/com/example/springboot3/jpa/h2/web/HomeControllerTest.java
+++ src/test/java/com/example/springboot3/jpa/h2/web/HomeControllerTest.java
@@ -4,6 +4,7 @@
 
 package com.example.springboot3.jpa.h2.web;
 
+import com.example.springboot3.jpa.h2.entity.Home;
 import com.example.springboot3.jpa.h2.service.HomeService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -13,6 +14,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -28,14 +30,27 @@ public class HomeControllerTest {
     private MockMvc mockMvc;
 
     @Test
-    @DisplayName("Get default resource Test")
+    @DisplayName("Get default resource [] Test")
     void test1() throws Exception {
-
         when(homeService.getAllHomies()).thenReturn(new ArrayList<>());
 
         mockMvc.perform(get("/homies"))
                 .andExpect(status().is2xxSuccessful())
                 .andExpect(content().contentType("application/json"))
-                .andExpect(jsonPath("$").isEmpty());
+                .andExpect(jsonPath("$").isArray());
+    }
+
+    @Test
+    @DisplayName("Get default resource [].* Test")
+    void test2() throws Exception {
+
+        List<Home> homeList = new ArrayList<>();
+        homeList.add(new Home(1, "Pac", Home.REL.DAD));
+        when(homeService.getAllHomies()).thenReturn(homeList);
+
+        mockMvc.perform(get("/homies"))
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(content().contentType("application/json"))
+                .andExpect(jsonPath("$.[0].name").value("Pac"));
     }
 }
